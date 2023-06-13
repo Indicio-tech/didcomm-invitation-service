f30712a80e3e1594b43c617a98fba0f7f7b2b20d
James Ebert <jamesebert.k@gmail.com>
2023-06-13 12:29:02 -0700

    feat: android intents launch didcomm scheme
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Aries Invitation Service
+
+## Android Intent
+Android Intents work on all the mobile browsers, some browsers will automatically prompt the user instead of requiring a button click on the website (which is preferred):
+
+* [ ] Android Chrome (if only one app is installed matching the scheme or if a specific package/app is defined, Android Chrome will prompt the user)
+* [x] Firefox
+* [ ] Opera
+* [x] Edge
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import QRCode from "react-qr-code";
 import './App.css'
 
 function App() {
@@ -6,12 +8,36 @@ function App() {
   const c_iQuery = queryParameters.get("c_i")
   const oobQuery = queryParameters.get("oob")
 
+  const [invitation, setInvitation] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    if(oobQuery){
+      setInvitation(oobQuery)
+    }
+    else if(c_iQuery){
+      setInvitation(c_iQuery)
+    }
+  }, [c_iQuery, oobQuery])
+
+  useEffect(() => {
+    window.location.assign('intent://testurl/#Intent;action=android.intent.action.CONNECT;scheme=didcomm;end');
+  }, [])
+  
 
   return (
     <>
       <h1>Aries Invitation Service</h1>
+      <a href="intent://testurl/#Intent;action=android.intent.action.CONNECT;scheme=didcomm;end">Invite!</a>
+      {invitation && 
+        <QRCode
+          size={256}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={invitation}
+          viewBox={`0 0 256 256`}
+        />
+      }
       <p className="created-by">
-        Created by James Ebert & Indicio
+        Created by James Ebert & Indicio 
       </p>
     </>
   )
